1a80afa84709a0e6c2e4a7448a95542074988354
Randall <ragrate@gmail.com>
2018-02-11 00:02:50 -0500

    Use the correct fragment
    Activity was incorrectly using the NumbersFragment

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -19,7 +19,7 @@ public class FamilyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.container, new NumbersFragment())
+                .replace(R.id.container, new FamilyFragment())
                 .commit();
     }
 
